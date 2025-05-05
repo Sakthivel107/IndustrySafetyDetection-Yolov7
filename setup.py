2385d662fed241c9676e43e977985c7fd180d785
Sakthivel <144320387+Sakthivel107@users.noreply.github.com>
2025-05-05 07:40:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'isd',
     version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'Sakthivel',
+    author_email= 'sakthivel107g@gmail.com',
     packages= find_packages(),
     install_requires = []
 
